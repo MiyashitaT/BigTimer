3683e635b9fabf7d8a8fcf3a3f97dd7957703ae8
MiyashitaT <eva1256list@gmail.com>
2021-04-04 14:50:42 +0900

    Initial Commit

---
+++ BigTimer/BigTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigTimerApp.swift
+//  BigTimer
+//
+//  Created by 宮下知也 on 2021/04/04.
+//
+
+import SwiftUI
+
+@main
+struct BigTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BigTimer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BigTimer
+//
+//  Created by 宮下知也 on 2021/04/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
